2693021d589440477be6625c4159ea7b20b35005
blackm-97 <blackm97@msu.edu>
2025-01-29 01:59:57 +0000

    Shortcode added

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("blog-post", function(title, paragraph){
+    return `<section class = "blog-post"> 
+<h1>${title}</h1> 
+<p>${paragraph}</p> 
+</section>`;
+  });
 };
